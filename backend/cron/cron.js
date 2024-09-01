4df56b9d546b7ebbd4512646e2be35c7bd660a53
01/aayu <shahaayush627@gmail.com>
2024-09-02 01:32:50 +0530

    backend and frontend link

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -1,7 +1,8 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://threads-clone-9if3.onrender.com";
+// const URL = "https://threads-clone-9if3.onrender.com";
+const URL  = "https://threads-frontend-azoq.onrender.com/";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
